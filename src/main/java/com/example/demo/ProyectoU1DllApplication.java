e317da777acbe604b7eacca961a4965a335da988
Daniel Llumiquinga <vladimirlestad@gmail.com>
2022-11-24 08:57:57 -0500

    Correccion

--- src/main/java/com/example/demo/ProyectoU1DllApplication.java
+++ src/main/java/com/example/demo/ProyectoU1DllApplication.java
@@ -1,15 +1,48 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// modificaicon
+import com.example.demo.spring.boot.CitaMedicaSB;
+import com.example.demo.spring.boot.MedicoSB;
+import com.example.demo.spring.boot.PacienteTerceraEdadSB;
+
+
 
 @SpringBootApplication
-public class ProyectoU1DllApplication {
+public class ProyectoU1DllApplication implements CommandLineRunner{
+	@Autowired
+	private PacienteTerceraEdadSB pacientTE;
+	
+	@Autowired
+	private CitaMedicaSB cita;
+	
+	@Autowired
+	private MedicoSB medico;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1DllApplication.class, args);
+		
+
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("SPRING BOOT");
+		System.out.println(pacientTE);
+		this.pacientTE.setCodigoIESS("15s57dwe52wds4");
+		this.pacientTE.setNombre("Edison");
+		this.pacientTE.setTipo("T");
+		this.pacientTE.setCedula("15458742512");
+		
+		System.out.println(pacientTE);
+		cita.agendar("123123", LocalDateTime.of(2022,12 ,2,8,30), pacientTE, medico);
+		
 	}
 
 }
